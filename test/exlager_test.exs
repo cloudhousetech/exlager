8bb9083be647fb58dbf6a5761e6f50f273a2a31d
rob <rob@cloudhouse.com>
2016-01-18 18:20:09 +0000

    Fixing test

--- test/exlager_test.exs
+++ test/exlager_test.exs
@@ -122,7 +122,7 @@ defmodule ExLager.Test do
 
   defp split(macros) do
     {e, d} = Enum.reduce macros, {[], []}, fn({level, res}, {e, d}) ->
-      if nil?(res) do
+      if res == nil do
         {e, [level|d]}
       else
         {[level|e], d}
